61b733074cde6c9c903a3ca996be1ef79a63c60d
Vijay Vepakomma <vijay.k.vepakomma@gmail.com>
2025-06-04 09:43:24 -0400

    add golang

---
+++ Arrays/src/main/golang/go.mod
@@ -0,0 +1,3 @@
+module vijayvepa/arrays
+
+go 1.24.2
---
+++ Arrays/src/main/golang/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	apartments := [][]string{
+		{"Apt 101", "Apt 102", "Apt 103"},
+		{"Apt 201", "Apt 202", "Apt 203"},
+		{"Apt 301", "Apt 302", "Apt 303"},
+	}
+
+	apartments[1][0] = "Vacant"
+
+	fmt.Printf("\n # Apartments: %v", apartments)
+
+	for floor, _ := range apartments {
+		fmt.Println("Floor", floor+1)
+		for room, _ := range apartments[floor] {
+			fmt.Println("Apartment", apartments[floor][room])
+		}
+		fmt.Println()
+	}
+}
