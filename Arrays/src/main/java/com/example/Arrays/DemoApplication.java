a6fcb5a69f1410bc0e131a71060f304a524fb3e6
Vijay Vepakomma <vijay.k.vepakomma@gmail.com>
2025-06-04 08:32:39 -0400

    make DimensionalArrays class for my curiosity

--- Arrays/src/main/java/com/example/Arrays/DemoApplication.java
+++ Arrays/src/main/java/com/example/Arrays/DemoApplication.java
@@ -1,13 +1,23 @@
 package com.example.Arrays;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		final int[][] example = DimensionalArrays.createExample();
+		System.out.println("First floor second item: " + example[0][1]);
+		DimensionalArrays.print(example);
+		DimensionalArrays.loop(example, (i,j, value)->
+				System.out.println("Floor "+ i + ", Room " + j + ", Value: " + value));
+	}
 }
