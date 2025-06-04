9688001184d27487d53b7f95b927875f9bb3103b
Vijay Vepakomma <vijay.k.vepakomma@gmail.com>
2025-06-04 14:59:25 -0400

    added csharp example

---
+++ Arrays/src/main/cs/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+string ArrayExample()
+{
+
+	string[][] array = [
+		["Apt 101", "Apt 102", "Apt 103"],
+		["Apt 201", "Apt 202", "Apt 203"],
+		["Apt 301", "Apt 302", "Apt 303"]
+	];
+	array[1][1] = "Vacant";
+	return string.Join(", ", array.SelectMany(x => x));
+}
+
+Console.WriteLine(ArrayExample());
