66ebbaa393918ee0de35f275191382c0fdbca58c
Vijay Vepakomma <vijay.k.vepakomma@gmail.com>
2025-06-04 08:46:54 -0400

    use logger

--- Arrays/src/main/java/com/example/Arrays/DemoApplication.java
+++ Arrays/src/main/java/com/example/Arrays/DemoApplication.java
@@ -1,11 +1,17 @@
 package com.example.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
+
+	static Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -14,10 +20,34 @@ public class DemoApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		final int[][] example = DimensionalArrays.createExample();
-		System.out.println("First floor second item: " + example[0][1]);
-		DimensionalArrays.print(example);
-		DimensionalArrays.loop(example, (i,j, value)->
-				System.out.println("Floor "+ i + ", Room " + j + ", Value: " + value));
+		final Integer[][] example = new Integer[][]{
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9}
+		}
+		;
+		log.info("First floor second item: {}", example[0][1]);
+		log.info(Arrays.deepToString(example));
+
+		DimensionalArrays.loop(example, (i, j, value)->
+				log.info("Floor: {}, Room: {}, Value: {} ", i, j, value));
+
+		final String[][] floors = {
+				{"Apt 101", "Apt 102", "Apt 103"},
+				{"Apt 201", "Apt 202", "Apt 203"},
+				{"Apt 301", "Apt 302", "Apt 303"}
+		};
+
+		StringBuilder stringBuilder = new StringBuilder();
+		DimensionalArrays.loop(floors, (i,j,value)-> stringBuilder.append(value).append(", "));
+		log.info("Floors: {}", stringBuilder);
+
+		//replace item
+		example[0][1] = 10;
+		log.info(Arrays.deepToString(example));
+
+		//number of floors:
+		System.out.println();
 	}
+
 }
--- Arrays/src/main/java/com/example/Arrays/DimensionalArrays.java
+++ Arrays/src/main/java/com/example/Arrays/DimensionalArrays.java
@@ -2,29 +2,14 @@ package com.example.Arrays;
 
 import org.apache.logging.log4j.util.TriConsumer;
 
-import java.util.Arrays;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-
 public class DimensionalArrays {
 
-  public static int[][] createExample() {
-    return new int[][]{
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
-    };
-  }
-
-  public static void print(int[][] array) {
-    System.out.println(Arrays.deepToString(array));
-  }
-
-  public static void loop(int[][] array, TriConsumer<Integer, Integer, Integer> action) {
+  public static <T> void loop(T[][] array, TriConsumer<Integer, Integer, T> action) {
     for (int i = 0; i < array.length; i++) {
       for (int j = 0; j < array[i].length; j++) {
         action.accept(i, j, array[i][j]);
       }
     }
   }
+
 }
